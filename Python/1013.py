e149a97e2074a7ad2eb40a3edd4a963713631aea
unknown <Henrique.lavarini2002@gmail.com>
2022-08-19 02:35:08 -0300

    Exercícios beecrowd com python

--- Python/1013.py
+++ Python/1013.py
@@ -1,4 +1,9 @@
-valor1 = int(input())
-valor2 = int(input())
-valor3 = int(input())
+import math
 
+valor = input().split(" ")
+valor1, valor2, valor3 = valor
+
+maior = (int(valor1) + int(valor2) + abs(int(valor1) - int(valor2)))  / 2
+resultado = (int(maior) + int(valor3) + abs(int(maior) - int(valor3)))/2
+
+print("%d eh o maior" %resultado)
