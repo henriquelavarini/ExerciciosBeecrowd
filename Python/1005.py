0ed913900c1595e20ca03a192b665b46e2f0d76e
unknown <Henrique.lavarini2002@gmail.com>
2022-08-19 13:11:00 -0300

    1005.py

--- Python/1005.py
+++ Python/1005.py
@@ -2,8 +2,8 @@
 nota1 = float(input())
 nota2 = float(input())
 
-#---------- Calculando média -----------
+#---------- Calculando média
 media = ((nota1 * 3.5) + (nota2 * 7.5))/11
 
-#---------- Apresentando --------------
+#---------- Apresentando
 print("MEDIA =", "%.5f"%media)
