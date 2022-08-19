6458a32cbc53b40245753923ea683f6ae611b94e
unknown <Henrique.lavarini2002@gmail.com>
2022-08-19 13:12:14 -0300

    1007.py

--- Python/1007.py
+++ Python/1007.py
@@ -4,8 +4,8 @@ valor2 = int(input())
 valor3 = int(input())
 valor4 = int(input())
 
-#----------------- Calculando a diferença entre o produto valor1 e valor2 pelo produto valor3 e valor4  --------------
+#----------------- Calculando a diferença entre o produto valor1 e valor2 pelo produto valor3 e valor4
 diferenca = ((valor1 * valor2) - (valor3 * valor4))
 
-#----------------- Apresentando ---------------
+#----------------- Apresentando
 print("DIFERENCA =", diferenca)
--- Python/1008.py
+++ Python/1008.py
@@ -3,11 +3,11 @@ numero = int(input())
 horas_trabalho = int(input())
 valor_hora_trabalho = float(input())
 
-#------------- Mostrando do numero do funcionario -----------------
+#------------- Mostrando do numero do funcionario
 print("NUMBER =", numero)
 
 #------------- Calculando o salario das horas de trabalho pelo valor da hora de trabalho
 salario = horas_trabalho * valor_hora_trabalho
 
-#------------- Mostrando o salário -----------------
+#------------- Mostrando o salário
 print("SALARY = U$", '%.2f'%salario)
