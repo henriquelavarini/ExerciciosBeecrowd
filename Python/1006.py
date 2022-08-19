726d3eec70455f6dae24e5fa6647e4104a13ba8c
unknown <Henrique.lavarini2002@gmail.com>
2022-08-19 13:11:28 -0300

    1006.py

--- Python/1006.py
+++ Python/1006.py
@@ -3,8 +3,8 @@ nota1 = float(input())
 nota2 = float(input())
 nota3 = float(input())
 
-#----------- Calculando média com peso (2, 3, 5) -------------
+#----------- Calculando média com peso (2, 3, 5)
 media = ((nota1 * 2) + (nota2 * 3) + (nota3 * 5))/(2+3+5)
 
-#----------- Apresentando --------------
+#----------- Apresentando
 print("MEDIA =",  "%.1f" % media)
