2c8620775093d4da5af418a4e89acbc24f080839
unknown <Henrique.lavarini2002@gmail.com>
2022-08-19 13:13:50 -0300

    1010.py

--- Python/1010.py
+++ Python/1010.py
@@ -1,17 +1,17 @@
-#---------- PEÇA 1 -----------
+#---------- PEÇA 1 ------------
 inteiro = input().split(" ")
 inteiro2 = input().split(" ")
 
 
 codigo_da_peca1, n_de_pecas1, valor_unitario1 = inteiro
 
-#---------- PEÇA 2 ------------
+#---------- PEÇA 2
 codigo_da_peca2, n_de_pecas2, valor_unitario2 =  inteiro2
 
 
 
-#---------- Calculando o preço para pagar ------------
+#---------- Calculando o preço para pagar
 total = ((int(n_de_pecas1) * float(valor_unitario1))) + ((int(n_de_pecas2) * float(valor_unitario2)))
 
-#---------- Mostrando ------------
+#---------- Mostrando
 print("VALOR A PAGAR: R$", '%.2f'%total)
