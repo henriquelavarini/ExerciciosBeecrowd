f42b796623fe9be476aa8caed70221c3c43254ca
unknown <Henrique.lavarini2002@gmail.com>
2022-08-28 03:19:54 -0300

    1011.py

---
+++ Python/1011.py
@@ -0,0 +1,11 @@
+#------------ Pegando informações
+raio = float(input())
+pi = 3.14159
+
+#----------- Calculando o volume
+
+volume = (4.0/3) * pi * raio**3
+
+#----------- Apresentando valor
+
+print("VOLUME =", '%.3f'%volume)
