9c6040810faef5abb4f6986aa45c91bb8a2c4697
unknown <Henrique.lavarini2002@gmail.com>
2022-08-28 13:29:30 -0300

    1016.py

---
+++ Python/1016.py
@@ -0,0 +1,8 @@
+#---------------- Pegando informações
+distancia = int(input())
+
+#---------------- Calculando o tempo
+tempo = (distancia * 2)
+
+#---------------- Apresentando
+print(tempo, "minutos")
